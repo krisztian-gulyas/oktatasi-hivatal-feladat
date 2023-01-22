7e91afea74a35c46be842cd96e767eb108d41144
krisztian-gulyas <krisztian.gulyas@outlook.com>
2023-01-22 12:57:13 +0100

    Update Kovetelmenyrendszer.php

--- src/model/Kovetelmenyrendszer.php
+++ src/model/Kovetelmenyrendszer.php
@@ -1,5 +1,59 @@
 <?php namespace App\Model;
 	class Kovetelmenyrendszer {
-		
+		private $egyetem = "";
+		private $kar = "";
+		private $szak = "";
+
+		private $kotelezo_targy = array();
+		private $kotelezoen_valaszthato_targy = array();
+
+		public function Egyetem($egyetem) {
+			$this->egyetem = $egyetem;
+			return $this;
+		}
+
+		public function egyetem() {
+			return $this->egyetem;
+		}
+
+		public function Kar($kar) {
+			$this->kar = $kar;
+			return $this;
+		}
+
+		public function kar() {
+			return $this->kar;
+		}
+
+		public function Szak($szak) {
+			$this->szak = $szak;
+			return $this;
+		}
+
+		public function szak() {
+			return $this->szak;
+		}
+
+		public function description() {
+			return ($this->egyetem ." ". $this->kar ." - ". $this->szak);
+		}
+
+		public function Kotelezo_targy($kotelezo_targy = array()) {
+			$this->kotelezo_targy = $kotelezo_targy;
+			return $this;
+		}
+
+		public function kotelezo_targy() {
+			return $this->kotelezo_targy;
+		}
+
+		public function Kotelezoen_valaszthato_targy($kotelezoen_valaszthato_targy = array()) {
+			$this->kotelezoen_valaszthato_targy = $kotelezoen_valaszthato_targy;
+			return $this;
+		}
+
+		public function kotelezoen_valaszthato_targy() {
+			return $this->kotelezoen_valaszthato_targy;
+		}
 	}
 ?>
