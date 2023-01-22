8c328e162bd8c8f10b9cdbf49cfd085805855a6d
krisztian-gulyas <krisztian.gulyas@outlook.com>
2023-01-22 12:13:52 +0100

    'homework_input.php'-ban megadott táblák összesítése + további kiegészítés

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
-	
+	$list = array(
+		$exampleData, $exampleData1, $exampleData2, $exampleData3, // ELTE
+		$my_exampleData, $my_exampleData1, $my_exampleData2, $my_exampleData3 // PPKE
+	);
 ?>
