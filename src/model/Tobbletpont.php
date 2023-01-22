5632d55d95de8202dffdb1e6f62359851f040f54
krisztian-gulyas <krisztian.gulyas@outlook.com>
2023-01-22 12:46:49 +0100

    Update Tobbletpont.php

--- src/model/Tobbletpont.php
+++ src/model/Tobbletpont.php
@@ -1,5 +1,34 @@
 <?php namespace App\Model;
 	class Tobbletpont {
+		private $kategoria;
+		private $tiups;
+		private $nyelv;
 
+		public function Kategoria($kategoria) {
+			$this->kategoria = $kategoria;
+			return $this;
+		}
+
+		public function kategoria() {
+			return $this->kategoria;
+		}
+
+		public function Tiups($tipus) {
+			$this->tipus = $tipus;
+			return $this;
+		}
+
+		public function tpus() {
+			return $this->tipus;
+		}
+
+		public function Nyelv($nyelv) {
+			$this->nyelv = $nyelv;
+			return $this;
+		}
+
+		public function nyelv() {
+			return $this->nyelv;
+		}
 	}
 ?>
