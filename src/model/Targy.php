5e4bbee27c7be3948c0ae54504d957b596bcc82a
krisztian-gulyas <krisztian.gulyas@outlook.com>
2023-01-22 12:28:06 +0100

    prop, set, get megadás

--- src/model/Targy.php
+++ src/model/Targy.php
@@ -1,3 +1,32 @@
-<?php namespace App\Model; 
-	class Targy {}
+<?php namespace App\Model;
+	class Targy {
+		private $nev = null;
+		private $tipus = null;
+		private $eredmeny = 0;
+
+		public function Nev($nev) {
+			$this->nev = $nev;
+		}
+
+		public function nev() {
+			return $this->nev;
+		}
+
+		public function Tiups($tipus) {
+			$this->tipus = $tipus;
+			return $this;
+		}
+
+		public function tipus() {
+			return $this->tipus;
+		}
+
+		public function Eredmeny($eredmeny) {
+			$this->eredmeny = intval(str_replace('%', '', $eredmeny));
+		}
+
+		public function eredmeny() {
+			return $this->eredmeny;
+		}
+	}
 ?>
