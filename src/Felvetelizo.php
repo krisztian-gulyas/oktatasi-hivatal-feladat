838db906cbc63b3ab7005cae2a22e42353a903c2
krisztian-gulyas <krisztian.gulyas@outlook.com>
2023-01-22 12:41:47 +0100

    Update Felvetelizo.php

--- src/Felvetelizo.php
+++ src/Felvetelizo.php
@@ -1,3 +1,23 @@
 <?php namespace App\Feladat;
-	class Felvetelizo {}
+	class Felvetelizo {
+		private Kovetelmenyrendszer $kovetelmeny;
+		private $erettsegi_targyak = array();
+		private $tobbletpontok = array();
+		private $kotelezo_erettsegi_vizsga_targyak = array();
+
+		public function Kovetelmeny($kovetelmeny) {
+			$this->kovetelmeny = $kovetelmeny;
+			return $this;
+		}
+
+		public function Erettsegi_targyak($erettsegi_targyak) {
+			$this->erettsegi_targyak = $erettsegi_targyak;
+			return $this;
+		}
+
+		public function Tobbletpontok($tobbletpontok) {
+			$this->tobbletpontok = $tobbletpontok;
+			return $this;
+		}
+	}
 ?>
