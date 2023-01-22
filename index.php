27d721a4df776aaeeb3f92f548d507ad45d414ee
krisztian-gulyas <krisztian.gulyas@outlook.com>
2023-01-22 12:14:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+	require './src/Felvetelizo.php';
+	
 	$list = array(
 		$exampleData, $exampleData1, $exampleData2, $exampleData3, // ELTE
 		$my_exampleData, $my_exampleData1, $my_exampleData2, $my_exampleData3 // PPKE
