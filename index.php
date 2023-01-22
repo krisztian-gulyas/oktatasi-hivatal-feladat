fec2b4ee041c012d6cb9a52d0d08e70701ae027b
krisztian-gulyas <krisztian.gulyas@outlook.com>
2023-01-22 12:18:30 +0100

    main folyamat előkészítése

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
 	require './src/Felvetelizo.php';
-	
+
+	/*\ START MAIN \*/
 	$list = array(
 		$exampleData, $exampleData1, $exampleData2, $exampleData3, // ELTE
 		$my_exampleData, $my_exampleData1, $my_exampleData2, $my_exampleData3 // PPKE
 	);
+
+	$felvetelizok = array();
+	foreach ($list as $k => $v) {
+		$felvetelizok[$k] = new App\Feladat\Felvetelizo($v);
+		print($felvetelizok[$k]);
+	}
+	/*\ END MAIN \*/
 ?>
